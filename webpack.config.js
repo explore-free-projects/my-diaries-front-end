bc95e1542bfe8d2c5ba2566a81c56f112e2edd02
Lakshmanan <uilaxman@gmail.com>
2020-03-31 01:26:47 +0530

    classname convension

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
                         loader: 'css-loader',
                         options: {
                             modules: {
-                                localIdentName: "fsu_[local]_[hash:base64:5]",
+                                localIdentName: "dwa_[name]_[local]",
                             },                                                      
                             sourceMap: true
                         }
