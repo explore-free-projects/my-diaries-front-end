7e77cec4f19e39a3ba56467a9bdb011cb82a8051
Lakshmanan <uilaxman@gmail.com>
2020-03-30 20:40:35 +0530

    webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
+const path = require( 'path' );
+module.exports = {
+   context: __dirname,
+   entry: './src/index.js',
+   output: {
+      path: path.resolve( __dirname, 'dist' ),
+      filename: "[name].bundle.js",
+		chunkFilename: "[id].bundle.js",
+      publicPath: 'public',
+   },
+   devServer: {
+      historyApiFallback: true
+   },
+   resolve: {
+      extensions: ['.js', '.jsx'],
+      modules: ['node_modules']
+   },
+   module: {
+      rules: [
+         {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader"
+            }
+         },
+         {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+         },
+         {
+            test: /\.(png|j?g|svg|gif)?$/,
+            use: 'file-loader'
+         }
+      ]
+   },
+   plugins: [
+      new HtmlWebPackPlugin({
+         template: path.resolve( __dirname, 'public/index.html' ),
+         filename: 'index.html'
+      })
+   ]
+};
