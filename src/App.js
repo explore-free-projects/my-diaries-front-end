f321ad45e15743af94c9f5e5cd6c2b819bf30700
Lakshmanan <uilaxman@gmail.com>
2020-03-30 20:03:24 +0530

    inital component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Button from './Components/Button'
 
 function App() {
   return (
     <div className="App">
-      Hello  react
+      <Button />
     </div>
   );
 }
---
+++ src/Components/Button/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <button>
+        Button
+      </button>
+     );
+  }
+}
+ 
+
+export default Button;
