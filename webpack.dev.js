e53179ca4e3b2cec2a9ff66145817673ecf661f2
Lakshmanan <uilaxman@gmail.com>
2020-04-09 00:21:04 +0530

    webpack class name load test

---
+++ webpack.dev.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+        chunkFilename: '[id].js',
+        publicPath: ''
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            components: path.resolve(__dirname, './src/Components')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: [
+                        [
+                            "babel-plugin-styled-components",
+                            { "ssr": true, "displayName": true, "fileName": false ,"preprocess": false }
+                        ]
+                    ]
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'style-loader' },
+                    { 
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: "dwa_[name]_[local]",
+                            }			
+                        }
+                     },
+                     { 
+                         loader: 'postcss-loader',
+                         options: {
+                             ident: 'postcss',
+                             plugins: () => [
+                                 autoprefixer({})
+                             ]
+                         }
+                      }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                loader: 'url-loader?limit=10000&name=img/[name].[ext]'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + '/public/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ]
+};
