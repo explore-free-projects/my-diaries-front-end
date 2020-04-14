7cbe9048d3f0bdd8de15739265cc7ddd5ad7a2d2
Lakshmanan <uilaxman@gmail.com>
2020-04-15 00:22:08 +0530

    inital design changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Router } from "react-router-dom";
-import { ThemeProvider } from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
 import history from "./history.js";
 import { Main, SideNav } from './Common/';
 import { themes } from './utils/theme';
@@ -16,6 +16,32 @@ const darkTheme = () => ({
   ...themes['dark'],
 })
 
+const ThemeToggle = styled.div `
+  position: fixed;
+  right: 20px;
+  top: 20px;
+  z-index: 1
+`;
+
+
+const ThemeToggleBtn = styled.button `
+  padding: 6px 9px;
+  border: 1px solid #fff;
+  font-size: 12px;
+  outline: 0;
+  background-color: ${props => (props.currentTheme === true) ? "#333" : "#ddd"};
+
+  &:first-child {
+    border-top-left-radius: 4px;
+    border-bottom-left-radius: 4px;
+  }
+
+  &:last-child {
+    border-top-right-radius: 4px;
+    border-bottom-right-radius: 4px;
+  }
+`
+
 function App() {
   const [theme, setTheme] = useState(lightTheme())
   const setDarkTheme = () => setTheme(darkTheme())
@@ -23,11 +49,23 @@ function App() {
 
   return (
     <Router history={history}>
+      <ThemeToggle>
+        <ThemeToggleBtn 
+          onClick={setLightTheme}
+          currentTheme={theme.type === 'light'}
+          >
+            Make it light
+        </ThemeToggleBtn>
+        <ThemeToggleBtn 
+          onClick={setDarkTheme}
+          currentTheme={theme.type === 'dark'}
+          >
+            Make it dark
+        </ThemeToggleBtn>
+      </ThemeToggle>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <SideNav/>
-        {/* <button onClick={setLightTheme}>Make it light</button>
-                <button onClick={setDarkTheme}>Make it dark</button> */}
         <Main/>
       </ThemeProvider>
     </Router>
