1188f0ba9632e1f8b20165146d529c30f796d926
Lakshmanan <uilaxman@gmail.com>
2020-05-12 12:14:08 +0530

    comma issue

--- src/Common/index.js
+++ src/Common/index.js
@@ -1,11 +1,11 @@
 import MainLayout from './MainLayout/MainLayout';
-import SideNav from './Sidenav/SideNav';
 import TopNav from './TopNav/TopNav';
 import EmptyState from './EmptyState/EmptyState';
+import SideNav from './Sidenav/SideNav';
 
 export {
   TopNav,
   EmptyState,
   MainLayout,
-  SideNav,
+  SideNav
 }
