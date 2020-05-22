ef472e09325e2b0ae7d5267183ce90aa9c4cc586
Lakshmanan <uilaxman@gmail.com>
2020-05-22 23:35:42 +0530

    index file changes

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,4 +1,4 @@
-import NewArticle from './NewArticle/NewArticle';
+import NewArticle from './Newarticle/Newarticle';
 import TextEditor from './TextEditor/TextEditor';
 import TextEditorPreview from './TextEditor/TextEditorPreview';
 import Loading from './Loading/Loading';
